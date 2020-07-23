5fdabe246d53d01d94d2bb6de656b636dc4cc6b5
Felipe <felipe.ortiz.san@gmail.com>
2020-07-23 19:51:45 -0400

    Texto desplegable en la parte superior

--- src/Vistas/AnalisisRepago.js
+++ src/Vistas/AnalisisRepago.js
@@ -1,28 +1,58 @@
 import React from "react";
 import TablaAnalisisRepago from '../components/TablaAnalisisRepago';
-import Divider from '@material-ui/core/Divider';
+import { Button, Accordion } from "@material-ui/core";
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      width: '100%',
+      backgroundColor: "#ffff",
+    },
+    heading: {
+      fontSize: theme.typography.pxToRem(15),
+      fontWeight: theme.typography.fontWeightRegular,
+    },
+}));
 
 const AnalisisRepago = () => {
     const hola = 'Vista analisi de repago';
+    const classes = useStyles();
 
     return(
         <>
-         <h1>
+        <h1>
              Tabla de análisis de repago
-         </h1>
+        </h1>
+
+        <div className={classes.root}>
+        <Accordion>
+            <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+            >
+            <Typography className={classes.heading}> <h3>Información</h3></Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+            <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                sit amet blandit leo lobortis eget.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                sit amet blandit leo lobortis eget.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                sit amet blandit leo lobortis eget.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                sit amet blandit leo lobortis eget.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                sit amet blandit leo lobortis eget.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                sit amet blandit leo lobortis eget.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                sit amet blandit leo lobortis eget.
+            </Typography>
+            </AccordionDetails>
+        </Accordion>
+        </div>
+         
          <div><TablaAnalisisRepago/></div>
-         <h1>
-             Información de la tabla
-         </h1>
-         <p>
-         Lorem ipsum dolor sit amet, consectetur 
-         adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Ut enim ad minim 
-          veniam, quis nostrud exercitation ullamco laboris 
-          nisi ut aliquip ¿por que sigues leyendo?. Duis aute 
-          irure dolor in reprehenderit in voluptate velit esse 
-          cillum dolore eu fugiat nulla pariatur. 
-         </p>
         </>
         
     );
