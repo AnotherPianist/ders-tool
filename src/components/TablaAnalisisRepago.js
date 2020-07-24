b8817c1e0b7b4039125b9ccc20da917c2a5e3785
Diego <diegovar36@gmail.com>
2020-07-24 16:00:11 -0400

    Añadido calculo automatico

--- src/components/TablaAnalisisRepago.js
+++ src/components/TablaAnalisisRepago.js
@@ -8,7 +8,10 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import TextField from "@material-ui/core/TextField";
-import { Button } from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+import AddIcon from "@material-ui/icons/Add";
+import RemoveIcon from "@material-ui/icons/Remove";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles({
   table: {
@@ -43,10 +46,16 @@ var id = 5;
  *  no se deberia editar debido a que es un valor que se calcula automaticamente} editable
  * @param {Valor inicial que tendra la celda (por defecto deberia ser 0)} valor
  */
-function crearElemento(nombre, editable, valor) {
-  return { nombre, editable, valor };
+function crearElemento(nombre, editable) {
+  return { nombre, editable };
 }
 
+/**
+ * Se generan los valores para cada espacio de las columnas, por defecto tienen valor 0.
+ *  */
+function generarValores() {
+  return [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+}
 /**
  * Funcion que añade una columna y calcula el año automaticamente
  */
@@ -54,70 +63,98 @@ function crearColumna() {
   id = id + 1;
   const texto = "Año " + id;
   const minWidth = 150;
-  return { id, texto, minWidth };
+  const valores = generarValores();
+  return { id, texto, minWidth, valores };
 }
-
-// var valorCostoAcumulado = 0;
-// var valorCostoDesarrollo = 0;
-
-//Por defecto se llega al año 5
-var columnas = [
-  { id: 0, texto: "Año " + 0, minWidth: 150 },
-  { id: 1, texto: "Año " + 1, minWidth: 150 },
-  { id: 2, texto: "Año " + 2, minWidth: 150 },
-  { id: 3, texto: "Año " + 3, minWidth: 150 },
-  { id: 4, texto: "Año " + 4, minWidth: 150 },
-  { id: 5, texto: "Año " + 5, minWidth: 150 },
-];
-
-//Datos que tendran las filas
-
 
 export default function TablaAnalisisRepago() {
   const classes = useStyles();
-  let [valorCostoAcumulado, setCostoD] = useState(0);
-  let [valorBeneAcumulado, setBeneficioAc] = useState(0);
-  let [velorBeneCosto, setBeneCosto] = useState(0);
-  let filasCosto = [
-    crearElemento("Costo Desarrollo", true, 0),
-    crearElemento("Costo Operacion", true, 0),
-    crearElemento("Factor Ajuste", true, 0),
-    crearElemento("Costos Ajustados", true, 0),
-    crearElemento("Costo Acumulado", false, valorCostoAcumulado),
+  const [columnas, setColumnas] = useState([
+    { id: 0, texto: "Año " + 0, minWidth: 150, valores: generarValores() },
+    { id: 1, texto: "Año " + 1, minWidth: 150, valores: generarValores() },
+    { id: 2, texto: "Año " + 2, minWidth: 150, valores: generarValores() },
+    { id: 3, texto: "Año " + 3, minWidth: 150, valores: generarValores() },
+    { id: 4, texto: "Año " + 4, minWidth: 150, valores: generarValores() },
+    { id: 5, texto: "Año " + 5, minWidth: 150, valores: generarValores() },
+  ]);
+
+  const filas = [
+    crearElemento("Costo Desarrollo", true),
+    crearElemento("Costo Operacion", true),
+    crearElemento("Factor Ajuste", true),
+    crearElemento("Costos Ajustados", false),
+    crearElemento("Costo Acumulado", false),
+    crearElemento("Beneficios", true),
+    crearElemento("Factor Ajuste", true),
+    crearElemento("Beneficio Ajustado", false),
+    crearElemento("Beneficio Acumulado", false),
+    crearElemento("Beneficio-Costo", false),
   ];
-  let filasBeneficio = [
-    crearElemento("Beneficios", true, 0),
-    crearElemento("Factor Ajuste", true, 0),
-    crearElemento("Beneficio Ajustado", true, 0),
-    crearElemento("Beneficio Acumulado", false, valorBeneAcumulado),
-  ];
-  let filaBeneCosto = [  
-    crearElemento("Beneficio-Costo", false, velorBeneCosto),
-  ];
-  const cambioCosto = (event) => {
-    valorCostoAcumulado = (valorCostoAcumulado)+parseInt(event.target.value);
-    setCostoD(valorCostoAcumulado);   
-    BeneficioCosto();
-  };
-  const cambioBeneficio = (event) => {
-    valorBeneAcumulado = (valorBeneAcumulado)+parseInt(event.target.value);
-    setBeneficioAc(valorBeneAcumulado);    
-    BeneficioCosto();
-  };
-  const BeneficioCosto = () =>{
-    setBeneCosto(valorBeneAcumulado-valorCostoAcumulado);
+
+  /**
+   * Funcion que luego de apretar el boton calcula los elementos haciendo las operaciones
+   * columna por columna
+   */
+  function calcularValores() {
+    let newArr = [...columnas];
+    var costeTotal = 0;
+    var beneficioTotal = 0;
+    newArr.map((columna) => {
+      //Calculos
+      var costo =
+        parseInt(newArr[columna.id].valores[1]) +
+        parseInt(newArr[columna.id].valores[0]);
+      var costoAño = costo * (newArr[columna.id].valores[2] / 100) + costo;
+      var beneficio = parseInt(newArr[columna.id].valores[5]);
+      var beneficioAño =
+        parseInt(beneficio) -
+        parseInt(beneficio) * (newArr[columna.id].valores[6] / 100);
+      costeTotal += costoAño;
+      beneficioTotal += parseInt(beneficioAño);
+      //Verificar que sean valores validos para hacer los cambios en la tabla
+      if (
+        !isNaN(costeTotal) &&
+        !isNaN(costoAño) &&
+        !isNaN(beneficioTotal) &&
+        !isNaN(beneficioAño)
+      ) {
+        newArr[columna.id].valores[3] = costoAño;
+        newArr[columna.id].valores[4] = costeTotal;
+        newArr[columna.id].valores[7] = beneficioAño;
+        newArr[columna.id].valores[8] = beneficioTotal;
+        newArr[columna.id].valores[9] = beneficioTotal - costeTotal;
+        setColumnas(newArr);
+      } else {
+        console.log(
+          "Hubo un error, posiblemente no hayan valores en las celdas de las columnas anteriores o ingresaste un valor invalido"
+        );
+      }
+    });
   }
+
+  /**
+   * Añade una columna vacia al final
+   */
   const agregarColumna = () => {
-    columnas.push(crearColumna());
+    let newState = [...columnas, crearColumna()];
+    setColumnas(newState);
   };
 
   return (
-    <Paper className={classes.root}>
-      <TableContainer className={classes.container}>
-        <Table stickyHeader aria-label="sticky table">
+    <Paper className={classes.root} key="Paper">
+      <TableContainer className={classes.container} key="Contenedor">
+        <Table stickyHeader aria-label="sticky table" key="Tabla">
           <TableHead>
             <TableRow>
-              <StyledTableCell>Elemento</StyledTableCell>
+              <StyledTableCell>
+                <IconButton aria-label="add" onClick={agregarColumna}>
+                  <AddIcon />
+                </IconButton>
+                <IconButton aria-label="remove" onClick={agregarColumna}>
+                  <RemoveIcon />
+                </IconButton>
+                Elemento
+              </StyledTableCell>
               {columnas.map((columna) => (
                 <StyledTableCell
                   key={columna.id}
@@ -130,9 +167,9 @@ export default function TablaAnalisisRepago() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {filasCosto.map((fila) => {
+            {filas.map((fila, index) => {
               return (
-                <StyledTableRow hover role="checkbox" tabIndex={-1} key={fila}>
+                <StyledTableRow hover role="checkbox" tabIndex={-1} key={index}>
                   <StyledTableCell component="th" scope="fila">
                     {fila.nombre}
                   </StyledTableCell>
@@ -141,49 +178,27 @@ export default function TablaAnalisisRepago() {
                       <StyledTableCell key={columna.id}>
                         <TextField
                           id="standard-basic"
-                          onChange={cambioCosto}
-                          label={!fila.editable ? fila.valor : "Ingresar Valor"}
-                          disabled={!fila.editable}
-                        />
-                      </StyledTableCell>
-                    );
-                  })}
-                </StyledTableRow>
-              );
-            })}
-            {filasBeneficio.map((fila) => {
-              return (
-                <StyledTableRow hover role="checkbox" tabIndex={-1} key={fila}>
-                  <StyledTableCell component="th" scope="fila">
-                    {fila.nombre}
-                  </StyledTableCell>
-                  {columnas.map((columna) => {
-                    return (
-                      <StyledTableCell key={columna.id}>
-                        <TextField
-                          id="standard-basic"
-                          onChange={cambioBeneficio}
-                          label={!fila.editable ? fila.valor : "Ingresar Valor"}
-                          disabled={!fila.editable}
-                        />
-                      </StyledTableCell>
-                    );
-                  })}
-                </StyledTableRow>
-              );
-            })}
-            {filaBeneCosto.map((fila) => {
-              return (
-                <StyledTableRow hover role="checkbox" tabIndex={-1} key={fila}>
-                  <StyledTableCell component="th" scope="fila">
-                    {fila.nombre}
-                  </StyledTableCell>
-                  {columnas.map((columna) => {
-                    return (
-                      <StyledTableCell key={columna.id}>
-                        <TextField
-                          id="standard-basic"                         
-                          label={!fila.editable ? fila.valor : "Ingresar Valor"}
+                          onChange={(e) => {
+                            if (
+                              parseInt(e.target.value) < 0 ||
+                              isNaN(e.target.value)
+                            ) {
+                              let newArr = columnas;
+                              newArr[columna.id].valores[index] =
+                                e.target.value;
+                              setColumnas(newArr);
+                            } else {
+                              let newArr = columnas;
+                              newArr[columna.id].valores[index] =
+                                e.target.value;
+                              setColumnas(newArr);
+                            }
+                          }}
+                          label={
+                            !fila.editable
+                              ? columnas[columna.id].valores[index]
+                              : "Ingresar Valor"
+                          }
                           disabled={!fila.editable}
                         />
                       </StyledTableCell>
@@ -193,12 +208,11 @@ export default function TablaAnalisisRepago() {
               );
             })}
           </TableBody>
+          <Button variant="contained" color="primary" onClick={calcularValores}>
+            Calcular
+          </Button>
         </Table>
       </TableContainer>
-      <Button variant="contained" color="primary" onClick={agregarColumna}>
-        Boton en version inicial para agregar años(Hay que ingresar a otra
-        pagina y volver a esta para ver cambios)
-      </Button>
     </Paper>
   );
 }
