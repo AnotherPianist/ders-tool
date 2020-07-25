fe8fca27c89c94b157121ebb916e1488c539c0ea
Carlos Daniel Ríos Moya <cafarimo@outlook.com>
2020-07-25 00:41:45 -0400

    Structure v1.0

--- src/App.js
+++ src/App.js
@@ -1,140 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { withStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import IconButton from '@material-ui/core/IconButton';
-import Drawer from '@material-ui/core/Drawer';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import Divider from '@material-ui/core/Divider';
-import ListaHerramientas from './components/ListaHerramientas.js';
-import TopBar from './components/topBar.js';
+import Estructura from './components/estructura/Estructura.js';
 import { Switch, Route } from 'react-router-dom';
-
-const drawerWidth = 240;
-
-const styles = theme => ({
-  root: {
-    display: 'flex',
-  },
-  toolbar: {
-    paddingRight: 24, // keep right padding when drawer closed
-  },
-  toolbarIcon: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-  },
-  appBar: {
-    backgroundColor: "white !important",
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  appBarSpacer: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    height: '100vh',
-    overflow: 'auto',
-  },
-  menuButton: {
-    marginRight: 36,
-  },
-  menuButtonHidden: {
-    display: 'none',
-  },
-  title: {
-    flexGrow: 1,
-  },
-  drawerPaper: {
-    backgroundColor: '#4e73df !important',
-    position: 'relative',
-    whiteSpace: 'nowrap',
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerPaperClose: {
-    overflowX: 'hidden',
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    width: theme.spacing(7),
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9),
-    },
-  },
-  content: {
-    flexGrow: 1,
-    height: '100vh',
-    overflow: 'auto',
-  },
-  white:{
-    color: 'white'
-  }
-});
+import { CssBaseline } from '@material-ui/core';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      appbarOpen: true,
-    }
-  }
-
-  handleDrawerOpen = () => {
-    this.setState({ open: true });
-  }
-
-  handleDrawerClose = () => {
-    this.setState({ open: false });
+    this.state = {};
   }
 
   render() {
-    const { classes } = this.props;
-
     return (
-      <div className={classes.root}>
+      <div style={{display: "flex"}}>
         <CssBaseline />
-        <TopBar
-          appBarClass={clsx(classes.appBar, this.state.open && classes.appBarShift)}
-          toolBarClass={classes.toolbar}
-          handleDrawerOpen={this.handleDrawerOpen}
-          iconClass={clsx(classes.menuButton, this.state.open && classes.menuButtonHidden)}
-        />
-
-        <Drawer
-          variant="permanent"
-          classes={{
-            paper: clsx(classes.drawerPaper, !this.state.open && classes.drawerPaperClose),
-          }}
-          open={this.state.open}
-        >
-          <div className={classes.toolbarIcon}>
-            <IconButton className={classes.white} onClick={this.handleDrawerClose}>
-              <ChevronLeftIcon/>
-            </IconButton>
-          </div>
-          <Divider />
-          <ListaHerramientas />
-        </Drawer>
-        <main className={classes.content}>
-          <div className={classes.appBarSpacer} />
+        <Estructura />
+        <main>
           <Switch>
             {/* Ejemplos de uso de Route path: */}
             <Route path="/ders">
@@ -154,7 +35,8 @@ class App extends React.Component {
             </Route>
           </Switch>
         </main>
-      </div>);
+      </div>
+    );
   }
 }
 
@@ -162,4 +44,4 @@ App.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(App);
+export default App;
