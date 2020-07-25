7da0703813e5261cd413191e84e209dbb69de57a
Felipe <felipe.ortiz.san@gmail.com>
2020-07-25 19:43:57 -0400

    Texto desplegable y guardado de datos

--- src/components/TablaAnalisisRepago.js
+++ src/components/TablaAnalisisRepago.js
@@ -139,6 +139,8 @@ export default function TablaAnalisisRepago() {
       } else {
         console.log("Ingresaste un porcentaje invalido");
       }
+      
+      console.log(columnas);
     });
   }
 
