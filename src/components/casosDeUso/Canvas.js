a95e1db13501a2ea701f9d698b39fe8498fe2280
Camilo Villalobos <camilo.svs@gmail.com>
2020-07-26 20:57:25 -0400

    Update canvas.js. Nuevas lineas de tipo dependencia, include y extend

--- src/components/casosDeUso/Canvas.js
+++ src/components/casosDeUso/Canvas.js
@@ -8,19 +8,17 @@ class Canvas extends React.Component {
     super(props);
 
     this.state = {
-      flecha: [
-        {
-          x1: 0,
-          y1: 0,
-          x2: 0,
-          y2: 0,
-        },
-      ],
+      //Flecha1 contiene las flechas normales
+      lineasSolidas: [],
+      //Flecha2 contiene las flechas punteadas de extend e include
+      lineasPunteadas: [],
+      //Arreglo de los puntos iniciales y finales de una linea
       posLinea: [
         { x: 0, y: 0 },
         { x: 0, y: 0 },
       ],
-      linea: false,
+      dibujarLinea: false,
+      tipo: 0,
       nroClick: 0,
       resetClick: false,
     };
@@ -67,42 +65,163 @@ class Canvas extends React.Component {
     );
   };
 
+  dibujarBotonAux = (name, tipo) => {
+    return (
+      <Button
+        onClick={() => {
+          console.log("click en linea1");
+          this.setState({ dibujarLinea: true });
+          this.setState({ tipo: tipo });
+        }}
+        color="primary"
+        variant="outlined"
+      >
+        {name}
+      </Button>
+    );
+  };
+  dibujarLineaPunteada = (i) => {
+    return (
+      <Line
+        points={[
+          this.state.lineasPunteadas[i].x1,
+          this.state.lineasPunteadas[i].y1,
+          this.state.lineasPunteadas[i].x2,
+          this.state.lineasPunteadas[i].y2,
+        ]}
+        dash={[5, 5, 0.001, 1]}
+        tension={1}
+        closed
+        stroke="black"
+        lineJoin="round"
+      />
+    );
+  };
+  dibujarLineaNormal = (i) => {
+    return (
+      <Line
+        points={[
+          this.state.lineasSolidas[i].x1,
+          this.state.lineasSolidas[i].y1,
+          this.state.lineasSolidas[i].x2,
+          this.state.lineasSolidas[i].y2,
+        ]}
+        tension={1}
+        closed
+        stroke="black"
+      />
+    );
+  };
+  calcularpuntomedio = (pto1, pto2) => {
+    return (pto1 + pto2) / 2;
+  };
+  dibujarEtiquetaLinea = (i) => {
+    return (
+      <Text
+        x={this.calcularpuntomedio(
+          this.state.lineasPunteadas[i].x1,
+          this.state.lineasPunteadas[i].x2
+        )}
+        y={this.calcularpuntomedio(
+          this.state.lineasPunteadas[i].y1,
+          this.state.lineasPunteadas[i].y2
+        )}
+        fontSize={17}
+        fontStyle="italic"
+        text={this.state.lineasPunteadas[i].etiqueta}
+        wrap="char"
+        align="center"
+      />
+    );
+  };
+  /* Funcion para cuando se clickea en una figura, si el modo linea esta activado (linea=true) se procesaran 2 clicks de mouse:
+  nroClick = 0 es el primer click que define el inicio de la linea,
+  nroClick = 1 es el segundo clicl que define el fin de la linea.
+  */
+  definirLinea = (e) => {
+    if (this.state.nroClick === 0) {
+      console.log("click1..");
+      let array = this.state.posLinea;
+      array[0].x = e.currentTarget.attrs.x;
+      array[0].y = e.currentTarget.attrs.y;
+      array[1].x = e.currentTarget.attrs.x;
+      array[1].y = e.currentTarget.attrs.y;
+      this.setState({ posLinea: array });
+    }
+    if (this.state.nroClick === 1) {
+      console.log("click2..");
+      let array = this.state.posLinea;
+      array[0].x = e.currentTarget.attrs.x;
+      array[0].y = e.currentTarget.attrs.y;
+      array[1].x = this.state.posLinea[1].x;
+      array[1].y = this.state.posLinea[1].y;
+      this.setState({ posLinea: array });
+      this.setState({ resetClick: true });
+    }
+    this.setState({ nroClick: this.state.nroClick + 1 });
+    /**Cuando se alcanza el segundo click se resetean las variables, 
+    se desactiva del modo linea y se guarda la linea obtenida*/
+    if (this.state.resetClick) {
+      console.log("reseteando..");
+      this.setState({ nroClick: 0 });
+      this.setState({ resetClick: false });
+      this.setState({ dibujarLinea: false });
+      var newFlecha = {
+        x1: this.state.posLinea[0].x,
+        y1: this.state.posLinea[0].y,
+        x2: this.state.posLinea[1].x,
+        y2: this.state.posLinea[1].y,
+        etiqueta: "",
+      };
+      if (this.state.tipo === 0) {
+        var flecha1 = this.state.lineasSolidas;
+        flecha1.push(newFlecha);
+        this.setState({
+          flecha1: flecha1,
+        });
+      } else {
+        var flecha2 = this.state.lineasPunteadas;
+        if (this.state.tipo === 1) {
+          newFlecha.etiqueta = "<<i>>";
+        } else {
+          newFlecha.etiqueta = "<<e>>";
+        }
+
+        flecha2.push(newFlecha);
+        this.setState({
+          flecha2: flecha2,
+        });
+      }
+    }
+  };
   render() {
     return (
       <div>
-        {/* Boton para activar-desactivar el modo linea, si linea = true 
-        la funcionalidad onClick dibuja, si es false no lo hace*/}
-        <Button
-          onClick={() => {
-            let { linea } = this.state;
-            linea = !linea;
-            this.setState({ linea });
-            console.log(this.state.linea);
-          }}
-          color="primary"
-          variant="outlined"
-        >
-          LINEA
-        </Button>
-
+        {/* Botones temporales para activar-desactivar el modo linea, si linea = true 
+        la funcionalidad onClick dibuja, si es false no lo hace. Hay varios botones segun el 
+        tipo de la linea*/}
+        {this.dibujarBotonAux("Linea", 0)} {/*LINEA NORMAL*/}
+        {this.dibujarBotonAux("Linea_inc", 1)} {/*LINEA TIPO INCLUDE*/}
+        {this.dibujarBotonAux("Linea_ext", 2)} {/*LINEA TIPO EXTEND */}
         <Stage width={window.innerWidth} height={window.innerHeight}>
-          {/** Ciclo para dibujar lineas */}
-          {[...Array(this.state.flecha.length)].map((_, i) => (
+          {/** Ciclo para dibujar lineas normales */}
+
+          {[...Array(this.state.lineasSolidas.length)].map((_, i) => (
             <Layer key={i} draggable>
               {/** Linea individual, obtenido desde el arreglo de flechas*/}
-              <Line
-                points={[
-                  this.state.flecha[i].x1,
-                  this.state.flecha[i].y1,
-                  this.state.flecha[i].x2,
-                  this.state.flecha[i].y2,
-                ]}
-                tension={1}
-                closed
-                stroke="black"
-              />
+              {this.dibujarLineaNormal(i)}
             </Layer>
           ))}
+
+          {/** Ciclo para dibujar lineas punteadas include y extend */}
+          {[...Array(this.state.lineasPunteadas.length)].map((_, i) => (
+            <Layer key={i} draggable>
+              {/** Linea individual, obtenido desde el arreglo de flechas*/}
+              {this.dibujarLineaPunteada(i)}
+              {this.dibujarEtiquetaLinea(i)}
+            </Layer>
+          ))}
+
           {[...Array(this.props.figuras.length)].map((_, i) => (
             <Layer
               key={i}
@@ -112,46 +231,8 @@ class Canvas extends React.Component {
               draggable
               onDragEnd={(e) => this.props.actualizarCoordenadas(e)}
               onClick={(e) => {
-                /* Funcion para cuando se clickea en una figura, si el modo linea esta activado (linea=true) se procesaran 2 clicks de mouse:
-                nroClick = 0 es el primer click que define el inicio de la linea,
-                nroClick = 1 es el segundo clicl que define el fin de la linea.
-                 */
-                if (this.state.linea) {
-                  if (this.state.nroClick === 0) {
-                    let array = this.state.posLinea;
-                    array[0].x = e.currentTarget.attrs.x;
-                    array[0].y = e.currentTarget.attrs.y;
-                    array[1].x = array[0].x;
-                    array[1].y = array[0].y;
-                    this.setState({ posLinea: array });
-                  }
-                  if (this.state.nroClick === 1) {
-                    let array = this.state.posLinea;
-                    array[1].x = this.state.posLinea[1].x;
-                    array[1].y = this.state.posLinea[1].y;
-                    array[0].x = e.currentTarget.attrs.x;
-                    array[0].y = e.currentTarget.attrs.y;
-                    this.setState({ posLinea: array });
-                    this.setState({ resetClick: true });
-                  }
-                  this.setState({ nroClick: this.state.nroClick + 1 });
-                  /**Cuando se alcanza el segundo click se resetean las variables, se desactiva del modo linea y se guarda la linea obtenida,
-                   */
-                  if (this.state.resetClick) {
-                    this.setState({ nroClick: 0 });
-                    this.setState({ resetClick: false });
-                    this.setState({ linea: false });
-                    var flecha = this.state.flecha;
-                    flecha.push({
-                      x1: this.state.posLinea[0].x,
-                      y1: this.state.posLinea[0].y,
-                      x2: this.state.posLinea[1].x,
-                      y2: this.state.posLinea[1].y,
-                    });
-                    this.setState({
-                      flecha: flecha,
-                    });
-                  }
+                if (this.state.dibujarLinea) {
+                  this.definirLinea(e);
                 }
               }}
             >
