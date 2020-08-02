aabc28af4388f83bc79a491a4fd491340d690634
lucianoags <lnicolas15@alumnos.utalca.cl>
2020-08-01 22:44:23 -0400

    Add files via upload

--- src/components/previsualizacion/BarraPDF.js
+++ src/components/previsualizacion/BarraPDF.js
@@ -1,18 +1,58 @@
 import React from 'react';
+import {Container, List, ListItem, ListItemIcon, Checkbox, ListItemText} from '@material-ui/core'
+
+
 
 class BarraPDF extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      DERS: false,
+      ListadeRequisitos: false,
+      TarjetasdeVolere: false,
+      Casosdeuso: false,
+      AjustesAmbientales: false,
+      PuntosdeFunción: false,
+      AnálisisdeRepago: false
+
+
+    };
+  };
+
+  handleToggle = (value) => () => {
+    value = value.replace(/\s/g, '');
+      var check = this.state[value];
+      if (check === false)
+        this.setState({[value]: true});
+      else
+        this.setState({[value]: false});
   }
 
   render() {
-    return(
-      <>
-        <h1>BarraPDF (esto está solo para que no tire error, ustedes bórrenlo)</h1>
-      </>
+
+    return (
+
+      <List className={this.props.useStyles}>
+        {["DERS", "Lista de Requisitos", "Tarjetas de Volere", "Casos de uso", "Ajustes Ambientales", "Puntos de Función", "Análisis de Repago"].map((value) => {
+          const labelId = `checkbox-list-label-${value}`;
+
+          return(
+            <ListItem key={value} role={undefined} dense button onClick={this.handleToggle(value)}>
+              <ListItemIcon >
+                <Checkbox
+                  checked = {this.state[value.replace(/\s/g, '')]}
+                  edge="start"
+                  tabIndex={-1}
+                  disableRipple
+                />
+              </ListItemIcon >
+              <ListItemText primary={value}/>
+            </ListItem>
+          );
+        })}
+      </List>
     );
   }
-
 }
 
 export default BarraPDF;
