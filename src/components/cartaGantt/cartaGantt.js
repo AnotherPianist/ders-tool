df9b6f41392a5f68be175c81a094dc3237b0bf68
Sebastian Cancino <sebcan1998@gmail.com>
2020-08-09 01:18:43 -0400

    Tiempo de tarea RU limitado por sus RS básico

--- src/components/cartaGantt/cartaGantt.js
+++ src/components/cartaGantt/cartaGantt.js
@@ -31,43 +31,36 @@ class CartaGantt extends React.Component {
         return color;
     }
 
-    onUpdateTask = (item, props) => {
-        item.start = props.start ? props.start : item.start;
-        item.end = props.end ? props.end : item.end;
-        this.setState({ data: [...this.state.data] });
+    onUpdateTask = async (item, props) => {
+        if (!item.isRU) {
+            item.start = props.start ? props.start : item.start;
+            item.end = props.end ? props.end : item.end;
+            await this.setState({ data: [...this.state.data] });
+            this.updateParentTaskDate(item);
+        }
     };
 
     onSelectItem = item => {
         this.setState({ selectedItem: item });
     };
 
-    addTask = (taskName, rid) => {
-        let newTask = {
-            id: `${this.state.data.length + 1}-${new Date().getTime()}`,
-            start: new Date(),
-            end: this.getEndDate(),
-            reqId: rid,
-            name: taskName,
-            color: this.getRandomColor()
-        };
-        this.setState(prevState => ({ data: [newTask, ...prevState.data] }));
-        console.log("Created task ", taskName);
-    };
-
-    createTask = (taskName, rid) => {
+    createTask = (taskName, req) => {
         return {
             id: `${this.state.data.length + 1}-${new Date().getTime()}`,
             start: new Date(),
             end: this.getEndDate(),
-            reqId: rid,
+            reqKey: req.key,
+            reqId: req.id,
             name: taskName,
-            color: this.getRandomColor()
+            color: this.getRandomColor(),
+            isRU: req.isRU,
+            refRU: req.refRU,
         };
     };
 
-    updateTask = (task) => {
-        let newName = this.props.requisitos.find(item => item.key === task.reqId).nombre;
-        return { id: task.id, start: task.start, end: task.end, task: task.reqId, name: newName, color: task.color };
+    updateTaskAttributes = (task) => {
+        let req = this.props.requisitos.find(item => item.key === task.reqKey);
+        return { ...task, reqId: req.id, name: req.nombre, refRU: req.refRU };
     };
 
     filterLink(newData, link) {
@@ -76,19 +69,45 @@ class CartaGantt extends React.Component {
         return start && end;
     }
 
+    shouldTaskUpdate(task, req) {
+        return (task.name !== req.nombre) || (task.refRU !== req.refRU) || (task.reqId !== req.id);
+    }
+
     updateCarta = () => {
         let { requisitos } = this.props;
         let { data, links } = this.props.cartaGantt;
 
         let newData = requisitos.map(req => {
-            let existingTask = data.find(task => task.reqId === req.key);
-            if (!existingTask) return this.createTask(req.nombre, req.key);
-            if (existingTask.name !== req.nombre) return this.updateTask(existingTask);
+            let existingTask = data.find(task => task.reqKey === req.key);
+            if (!existingTask) return this.createTask(req.nombre, req);
+            if (this.shouldTaskUpdate(existingTask, req)) return this.updateTaskAttributes(existingTask);
             return existingTask;
         });
 
         let newLinks = links.filter(link => this.filterLink(newData, link));
         this.setState({ data: newData, links: newLinks });
+    };
+
+    getParentTask = (task) => {
+        return this.state.data.find(t => t.isRU && t.reqId === task.refRU);
+    }
+
+    getChildrenTask = (parentTask) => {
+        return this.state.data.filter(t => !t.isRU && t.refRU === parentTask.reqId);
+    }
+
+    updateParentTaskDate = task => {
+        let parentTask = this.getParentTask(task);
+        let children = this.getChildrenTask(parentTask);
+        let dates = [];
+        children.forEach(child => dates.push(child.start, child.end));
+
+        const parentIndex = this.state.data.indexOf(parentTask);
+        let newData = [...this.state.data];
+        let newStart = Math.min(...dates);
+        let newEnd = Math.max(...dates);
+        newData[parentIndex] = { ...newData[parentIndex], start: newStart, end: newEnd };
+        this.setState({ data: newData });
     };
 
     componentWillUnmount = () => {
